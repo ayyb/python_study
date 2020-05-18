aa908e6fda805c698fa4567c1e3d7833aaaaefd4
ayyb <narabyeol@gmail.com>
2020-05-18 14:43:35 +0900

    변수와 입력

---
+++ string_operator01.py
@@ -0,0 +1,44 @@
+print("문자 선택 연산자에 대해 알아볼까요?")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+print("안녕하세요"[-1])
+
+print("안녕하세요"[-2])
+
+print("안녕하세요"[-3])
+
+print("안녕하세요"[-4])
+
+print("안녕하세요"[-5])
+
+print("안녕하세요"[1:4])
+print("안녕하세요"[1:])
+
+hello = "안녕하세요"
+print(hello[0:2])
+
+hello
+
+print(len("안녕하세요"))
+
+print(273)
+print(52.273)
+
+print(type(52))
+print(type(52.273))
+
+print(0)
+print(type(0))
+
+print("2**2=", 2**2)
+print("2**3=", 2 ** 3)
+
+print(("안녕" + "하세요") * 3)
+
+pi = 3.14159265
+pi
+
--- test.py
+++ test.py
@@ -16,3 +16,30 @@ print('"안녕하세요"라고 말했습니다')
 
 print("안녕하세요\n 안녕하세요")
 print("안녕하세요\t안녕하세요")
+
+print("""동해물과 백두산이 마르고 닳도록
+하느님이 보우하사 우리나라 만세
+무궁화 삼천리 화려강산 대한사람
+대한으로 길이 보전하세""")
+
+print(3 * "안녕하세요")
+
+
+number = 100
+number += 10
+number += 20
+number += 30
+print("number:", number)
+
+string = "안녕하세요"
+string += "!"
+string += "!"
+print("string:", string)
+
+input("인사말을 입력하세요>")
+
+string = input("인사말을 입력하세요>")
+print(string)
+
+number = input("숫자를 입력하세요 >")
+print(number)
