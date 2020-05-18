bbe88b4432c41a931bd4046d694aea07b622f8a5
ayyb <narabyeol@gmail.com>
2020-05-18 17:45:13 +0900

    리스트와 반복문

---
+++ pass_keyword01.py
@@ -0,0 +1,11 @@
+# 입력을 받습니다.
+number = input("정수 입력 >")
+number = int(number)
+
+# 조건문 사용
+if number > 0:
+    # 양수일 때: 아직 미구현 상태입니다.
+    pass  # 진짜로 아무것도 안함. 나중에 구현하겠음이라는 의미.
+else:
+    # 음수일 때: 아직 미구현 상태입니다.
+    raise NotImplementedError
