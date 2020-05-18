b7da36a65fefe95bd72de1d3f8d1a69a3eeb7b94
ayyb <narabyeol@gmail.com>
2020-05-18 13:44:39 +0900

    test.py 기본 테스트

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+import keyword
 print("TEST file 입니다.")
+print("Hello Coding Python")
+print("hello" * 3)
+print("Python Programming")
+
+print(keyword.kwlist)  # 주석처리
+
+print()  # 줄바꿈하기.
+print("안녕하세요", "저의", "이름은", "이명섭입니다.")
